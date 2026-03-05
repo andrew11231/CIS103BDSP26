f882311898d6896344248dee46d8ce76c65be793
andrew11231 <andrewjosue86@gmail.com>
2026-03-05 10:46:06 -0600

    Update P11 - AE.py

--- P11 - AE.py
+++ P11 - AE.py
@@ -1,15 +1,12 @@
 #Andrew Espinoza
 import time
-#def openfiles():
-   # genfiles = 'C:\temp\points.txt'
-    #datagenfiles = open(genfiles,'r')
-    #gradfiles = "C:\temp\gradess.txt"
-   # datagradfiles = open(gradfiles,'w')
-   # errorfiles = "C:\temp\errorrr.txt"
-   # dataerrorfiles = open(errorfiles,'w')
 
 genfiles = 'C:/temp/points.txt'
 datagenfiles = open(genfiles,'r')
+gradfiles = "C:/temp/gradess.txt"
+datagradfiles = open(gradfiles,'w')
+errorfiles = "C:/temp/errorrr.txt"
+dataerrorfiles = open(errorfiles,'w')
 
 line1 = datagenfiles.readline()
 print(line1)
