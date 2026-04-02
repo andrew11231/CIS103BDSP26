9fd5ad04823731e2e48c1d2db1562148e43bf346
andrew11231 <andrewjosue86@gmail.com>
2026-04-02 16:48:24 -0500

    Update P16 - A.E.py

--- P16 - A.E.py
+++ P16 - A.E.py
@@ -10,14 +10,18 @@ def procs(n):
     if n == 1:
         return "1"
     else:
-         return str(n) + '+' + summ(n-1)
+         return str(n) + '+' + procs(n-1)
 
-try:
-    num = int(input('Enter Number:'))
-    if num > 1:
-        print(procs(num)+' = '+ summ(num))
-    else:
-        print('Number must be positive')
-except:
-    print('Invalid Input Detected')
-    
+
+ans = 'y'
+while ans.lower() == 'y':
+    try:
+            print('\n')
+            num = int(input('Enter Number:'))
+            if num >= 1:
+                print(procs(num)+' = '+ str(summ(num)))
+                ans = input('y/n? = ')
+            else:
+                print('Number must be positive')
+    except:
+            print('Invalid Input Detected')
